6d2b6d427312ea9bdc0ef9116cf10d52691b2edc
laxmanPal <laxi27902@gmail.com>
2025-08-01 19:25:42 +0530

    update user details post api

--- frontend/src/pages/client/AccountDetails.jsx
+++ frontend/src/pages/client/AccountDetails.jsx
@@ -5,10 +5,11 @@ import { useAuth } from "../../store/AuthContext";
 import { useRef, useState } from "react";
 import { FiUploadCloud } from "react-icons/fi";
 import CircularProgress from "@mui/material/CircularProgress";
+import { toast } from "react-toastify";
 const API_URL = import.meta.env.VITE_API_URL;
 
 const AccountDetails = () => {
-  const { user, setUser } = useAuth();
+  const { user, setUser, fetchUserInfo } = useAuth();
   const { name, email, phone, avatar: userAvatar } = user;
   const fileInputRef = useRef(null);
   const [avatar, setAvatar] = useState(userAvatar.url || defaultAvatar);
@@ -19,7 +20,7 @@ const AccountDetails = () => {
     if (!file) return;
 
     // Preview image immediately
-    setAvatar(URL.createObjectURL(file));
+    // setAvatar(URL.createObjectURL(file));
 
     const formData = new FormData();
     formData.append("avatar", file);
@@ -41,22 +42,58 @@ const AccountDetails = () => {
           ...prevUser,
           avatar: { url: data.avatar },
         }));
+        toast.success("✅ Profile Update Successful!");
       }
-    } catch (err) {
-      console.error("Upload failed:", err);
+    } catch (error) {
+      console.error("Upload failed:", error);
       setAvatar(userAvatar?.url || defaultAvatar);
+      toast.error(`❌ ${error.message}`);
     } finally {
       setUploading(false);
     }
   };
+
+  const handleUserDetails = async (e) => {
+    e.preventDefault();
+    const fd = new FormData(e.target);
+    const formData = Object.fromEntries(fd.entries());
+    console.log(formData);
+
+    try {
+      const response = await fetch(`${API_URL}/user/update-user-detail`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+        credentials: "include",
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || "Login failed");
+      }
+
+      await fetchUserInfo();
+      e.target.reset();
+      console.log(data);
+      toast.success("✅ Account Details Update successful!");
+    } catch (error) {
+      toast.error(`❌ ${error.message}`);
+
+      console.error("Login error:", error.message);
+    }
+  };
+
   return (
     <div className="md:col-span-2 w-full h-full p-6 ">
       <div className="rounded-xl shadow-md overflow-hidden bg-white p-6 border border-gray-300 ">
         <h2 className="text-2xl font-semibold mb-6">Account Details</h2>
 
-        <form className="space-y-6">
-          {/* <!-- Name & Email --> */}
+        <form onSubmit={handleUserDetails} className="space-y-6">
           <div className="grid grid-cols-3 gap-6">
+            {/* Avatar */}
             <div className="col-span-1">
               <div className="relative group">
                 <img
@@ -68,7 +105,7 @@ const AccountDetails = () => {
 
                 {/* Hover Overlay */}
                 <div
-                  className="absolute inset-0 bg-black bg-opacity-50  flex items-center justify-center opacity-0 group-hover:opacity-60 transition-opacity cursor-pointer"
+                  className="absolute inset-0 bg-black bg-opacity-50  flex items-center justify-center opacity-0 group-hover:opacity-60 transition-opacity cursor-pointer text-white"
                   onClick={() => fileInputRef.current.click()}
                 >
                   {uploading ? (
@@ -87,6 +124,8 @@ const AccountDetails = () => {
                 />
               </div>
             </div>
+
+            {/* Details */}
             <div className="grid grid-cols-1  gap-6 col-span-2">
               <div>
                 <TextField
@@ -94,7 +133,9 @@ const AccountDetails = () => {
                   type="text"
                   id="name"
                   label="Name"
+                  name="name"
                   variant="outlined"
+                  required
                   defaultValue={name}
                 />
               </div>
@@ -105,7 +146,9 @@ const AccountDetails = () => {
                   id="email"
                   label="Email"
                   variant="outlined"
-                  disabled
+                  name="email"
+                  // disabled
+                  required
                   value={email}
                 />
               </div>
@@ -115,7 +158,9 @@ const AccountDetails = () => {
                   type="number"
                   id="phone"
                   label="Phone"
+                  name="phone"
                   variant="outlined"
+                  required
                   defaultValue={phone}
                 />
               </div>
@@ -124,7 +169,10 @@ const AccountDetails = () => {
           </div>
 
           {/* <!-- Update Button --> */}
-          <Button className="w-full  !bg-black !text-white !rounded-lg text-center font-medium gap-3 ">
+          <Button
+            type="submit"
+            className="w-full  !bg-black !text-white !rounded-lg text-center font-medium gap-3 "
+          >
             UPDATE PROFILE
           </Button>
         </form>
